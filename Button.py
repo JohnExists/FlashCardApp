95d402bde2207ba04bc065d9845cced75a99fbac
JohnExists <johnkolta9@gmail.com>
2024-01-12 09:07:49 -0500

    changed up menu design

--- Button.py
+++ Button.py
@@ -1,12 +1,13 @@
 import tkinter
 
+
 class Button():
 
     def __init__(self, text, x, y, width, height, window) -> None:
         self.button_frame = tkinter.Frame(window, highlightbackground="purple", highlightthickness=2)
 
         self.button = tkinter.Button(self.button_frame, text=text, width=width, height=height)
-        self.button.configure(bg="black", fg="#ac20fd", borderwidth=0, font= ('Helvetica', 15), padx=10, pady=10)
+        self.button.configure(bg="black", fg="#ac20fd", borderwidth=0, font=('Helvetica', 15), padx=10, pady=10)
         self.button.bind("<Enter>", lambda e: self.button.config(bg="#ac20fd", fg="black"))
         self.button.bind("<Leave>", lambda e: self.button.config(bg="black", fg="#ac20fd"))
         self.button.pack()
@@ -15,9 +16,9 @@ class Button():
 
     def center(self):
         self.button_frame.place(relx=0.5, rely=0.5, anchor="center")
-    
+
     def alignText(self, anchor):
         self.button.configure(anchor=anchor)
 
     def onClick(self, action):
-        self.button.configure(command= action)
+        self.button.configure(command=action)
--- Menu.py
+++ Menu.py
@@ -1,6 +1,7 @@
 import tkinter
 from Button import Button
 
+
 class Menu:
     def __init__(self, window) -> None:
         self.window = window
@@ -22,25 +23,45 @@ class Menu:
 
     def generateEntry(self, name, y, variable):
         border_color = tkinter.Frame(self.window, highlightbackground="purple", highlightthickness=2)
-        
+
         self.widgets[name] = tkinter.Entry(border_color, textvariable=variable)
-        self.widgets[name].configure(bg="black", fg="#ac20fd", width=25, font= ('Helvetica', 25), borderwidth = 0)
+        self.widgets[name].configure(bg="black", fg="#ac20fd", width=25, font=('Helvetica', 25), borderwidth=0)
         self.widgets[name].pack()
 
         border_color.place(relx=0.5, rely=y, anchor="center")
 
+    def generateLists(self, name, list):
+        border_color = tkinter.Frame(self.window, highlightbackground="purple", highlightthickness=2)
+        scrollbar = tkinter.Scrollbar(border_color)
+        scrollbar.pack(side=tkinter.RIGHT, fill=tkinter.Y)
 
+        listBox = tkinter.Listbox(border_color, yscrollcommand=scrollbar.set)
+        for item in list:
+            listBox.insert(tkinter.END, item)
+
+        border_color.place(relx=0.5, rely=0.5, anchor="center")
+        listBox.configure(background="black", foreground="purple",
+                          font=('Helvetica', 15), width=60, borderwidth=0)
+        listBox.pack()
+        self.widgets[name] = listBox
+
+        scrollbar.config(command=listBox.yview)
 
     def launchMainMenu(self):
         self.clear()
-        self.widgets["add-button"] = Button("+ Add", 0.2, 0.2, 20, 4, self.window)
+        self.widgets["add-button"] = Button("+ Add", 0.5, 0.2, 60, 1, self.window)
         self.widgets["add-button"].onClick(lambda: self.launchAddMenu())
 
-        i = 0
-        for group in self.flashGroups:
-            self.widgets[group] = Button(group, 0.5, 0.4 + (0.1 * i), 60, 1, self.window)
-            self.widgets[group].alignText('w')
-            i += 1
+        def onSelect(event):
+            widget = event.widget
+            selection = widget.curselection()
+            value = widget.get(selection[0])
+            print("selection:", selection, ": '%s'" % value)
+
+
+        self.generateLists("all-groups-list", self.flashGroups)
+        self.widgets["all-groups-list"].bind("<Double-Button-1>", onSelect)
+
 
     def launchAddMenu(self):
         self.clear()
@@ -49,9 +70,14 @@ class Menu:
         self.generateEntry("name-entry", 0.2, name)
 
         def addGroup():
-            if(name.get() != ""):
+            if (name.get() != ""):
                 self.flashGroups.append(name.get())
                 self.launchMainMenu()
 
         self.widgets["submit-button"] = Button("Add Group", 0.5, 0.5, 20, 4, self.window)
         self.widgets["submit-button"].onClick(lambda: addGroup())
+
+    def launchGroupMenu(self, name):
+        self.clear()
+        self.widgets["add-button"] = Button("+ Add FlashCard", 0.5, 0.2, 60, 1, self.window)
+        self.widgets["add-button"].onClick(lambda: self.launchAddMenu())
